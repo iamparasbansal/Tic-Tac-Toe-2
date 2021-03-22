9e861a897320a3021e11f6655fa1a80d982d904f
Paras Bansal <parasbansal685@gmail.com>
2021-03-22 11:15:32 +0530

    Game Complete, UI Managed!

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean gameActive = true;
+
     // 0 - X
     //1 - O
 
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     public void playerTap(View view){
         ImageView img = (ImageView) view;
         int tappedImage= Integer.parseInt(img.getTag().toString());
+        if(!gameActive){
+            gameReset(view);
+        }
         if(gameState[tappedImage] == 2){
             gameState[tappedImage] = activePlayer;
             img.setTranslationY(-1000f);
@@ -46,6 +51,44 @@ public class MainActivity extends AppCompatActivity {
             }
             img.animate().translationYBy(1000f).setDuration(300);
         }
+        //Check if any player has won
+        for(int[] winPosition: winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != 2) {
+                //somebody has won!- Find out who?
+                String winnerStr;
+                gameActive = false;
+                if (gameState[winPosition[0]] == 0) {
+                    winnerStr = "X has Won";
+                } else {
+                    winnerStr = "O has Won";
+                }
+                //Update Status bar for winner
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+            }
+        }
+    }
+
+    public void gameReset(View view){
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++ ) {
+            gameState[i] = 2;
+        }
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView9)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("O's turn - Tap to play");
     }
 
     @Override
